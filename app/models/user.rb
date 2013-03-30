24aba8dcab5f77b0549cf86a11049bcb57ea4ec4
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2013-03-30 15:43:04 -0700

    adding password and confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
 
   validates_presence_of :name
   validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
 end
