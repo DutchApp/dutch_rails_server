182cd4e5204eefce868cb8f6cf790514f77cf30f
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2013-03-30 14:24:31 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DutchRails::Application.config.secret_token = 'f2bc0d3b57962966f930eef7a9cc6fa02554997e2e81672d4df0101986079630e9fb11a1717cb9286c1088972618ebc99ac72c94a07433d11426351bac48dcc5'
