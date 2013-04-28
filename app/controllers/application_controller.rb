ae3c6a6a87db26efcf8d18af18afcc2cfd0e2306
Anuj Chaudhary <anujchaudhary@Anuj-Chaudharys-MacBook-Pro.local>
2013-04-27 18:58:43 -0700

    Initial checkin for jQueryTouch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :prepare_for_mobile
+
+  def mobile_device?
+  	puts "mobile_device?: mobile_param: #{session[:mobile_param]}"
+  	puts "user agent: #{request.user_agent}"
+  	if session[:mobile_param]
+  		session[:mobile_param] == "1"
+  	else
+  		request.user_agent =~ /Mobile|webOS/
+  	end
+  end
+  	helper_method :mobile_device?
+
+  	def prepare_for_mobile
+  		puts "prepare_for_mobile: mobile_param: #{session[:mobile_param]}"
+  		session[:mobile_param] = params[:mobile] if params[:mobile]
+  		request.format = :mobile if mobile_device?
+  	end
 end
